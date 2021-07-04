3ee9304c5f73290ee8555c12910d52ea1bf1a90d
David Barcenas <daveepro@outlook.com>
2021-07-04 15:44:35 -0500

    add template system

--- posts/views.py
+++ posts/views.py
@@ -1,37 +1,37 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from datetime import datetime
 
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Yésica Cortés',
+        'title': 'Mont Blac',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/300'
+        'photo': 'https://picsum.photos/200/300'
     },
     {
-        'name': 'Mont Blac',
-        'user': 'Yésica Cortés',
+        'title': 'Mont Blac',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/300'
+        'photo': 'https://picsum.photos/200/300'
     },
     {
-        'name': 'Mont Blac',
-        'user': 'Yésica Cortés',
+        'title': 'Mont Blac',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/300'
+        'photo': 'https://picsum.photos/200/300'
     },
 ]
 
 # Create your views here.
 def list_posts(request):
     """List existing posts"""
-    content = []
-
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <img src="{picture}"/>
-        """.format(**post))
-
-    return HttpResponse('<br />'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
