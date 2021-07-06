87b5156ae223b5b81c667a975245befc60124c6b
David Barcenas <daveepro@outlook.com>
2021-07-06 17:03:21 -0500

    register the profile in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
+from users.models import Profile
 from django.contrib import admin
+from users.models import Profile
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('website', 'phone_number')
+    list_filter = ('user__is_active', 'user__is_staff', 'created', 'modified')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name')
+     
