d665a4a51b85fb757418b9689f04c4030c91c8d3
David Barcenas <daveepro@outlook.com>
2021-07-07 11:24:35 -0500

    register post in admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from posts.models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'title', 'photo')
+    list_editable = ('title', 'photo')
+
+    fieldsets = (
+        ('Post', {
+            'fields': ('user', 'profile', 'title', 'photo')
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        }),
+    )
+
+    readonly_fields = ('created', 'modified')
