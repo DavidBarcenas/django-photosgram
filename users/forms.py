f43c0f1216a2d6ebe8ac5ba03e9d26affa05d1d2
David Barcenas <daveepro@outlook.com>
2021-07-08 18:08:27 -0500

    handle form errors

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 class Profileform(forms.Form):
-    website = forms.URLField(max_length=200, required=True,)
-    biography = forms.CharField(max_length=500, required=False)
-    phone_number = forms.CharField(max_length=20, required=False,error_messages={'max_length': 'Please enter your name'})
     picture  = forms.ImageField()
+    website = forms.URLField(max_length=200, required=True,)
+    biography = forms.CharField(max_length=250, required=False)
+    phone_number = forms.CharField(
+        max_length=20, 
+        required=False,
+        error_messages={
+            'max_length': 'The phone number field is for a maximum of 20 characters'
+        }
+    )
