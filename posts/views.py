102718f170e9edc04f3cc07e87313d523d0dc78b
David Barcenas <daveepro@outlook.com>
2021-07-12 19:57:36 -0500

    change card design, header and description

--- posts/views.py
+++ posts/views.py
@@ -18,7 +18,7 @@ class PostsFeedView(LoginRequiredMixin, ListView):
     template_name = 'posts/feed.html'
     model = Post
     ordering = ('-created')
-    paginate_by = 2
+    paginate_by = 30
     context_object_name = 'posts'
 
 
